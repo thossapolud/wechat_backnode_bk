fc714663a129392ad97855402c7f5702dbb333b7
Thossapol <76428440+thossapolud@users.noreply.github.com>
2021-03-24 13:49:49 +0700

    test
    test

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.use(function (req, res, next) { // แก้ Access-Control-Allow-Origin
     // Request headers you wish to allow
     res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
   
-  //  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   
     // Pass to next layer of middleware
     next();
